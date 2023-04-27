52a84f93c6286d04703b57b838ae9992c778d421
jhhonr144 <jhhonr144@gmail.com>
2023-04-27 09:40:36 -0500

    Sugerencia de palabras

--- app/src/main/java/com/example/atanke/ui/sugerirtraduccion/SugerirTraduccion.java
+++ app/src/main/java/com/example/atanke/ui/sugerirtraduccion/SugerirTraduccion.java
@@ -1,15 +1,16 @@
 package com.example.atanke.ui.sugerirtraduccion;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-import com.example.atanke.MainActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.atanke.R;
 import com.example.atanke.ui.login.Login;
 import com.example.atanke.ui.registrarse.Registrarse;
@@ -17,6 +18,8 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class SugerirTraduccion extends AppCompatActivity {
     AutoCompleteTextView idiomas;
+    Spinner  codIdioma;
+    EditText palabra,traduccion,pronunciacion;
     Button sugerir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,11 @@ public class SugerirTraduccion extends AppCompatActivity {
         setContentView(R.layout.sugerir_palabra_activity);
 
         sugerir = findViewById(R.id.btnSugerir);
+        codIdioma = findViewById(R.id.codIdioma);
+        palabra = findViewById(R.id.palabra);
+        traduccion = findViewById(R.id.traduccion);
+        pronunciacion = findViewById(R.id.pronunciacion);
+
 
         sugerir.setOnClickListener(view -> {
             mensaje(this);
