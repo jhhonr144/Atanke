917fa592e74922dc6b17c9d06f2af95a5c33936a
jhhonr144 <jhhonr144@gmail.com>
2023-05-05 21:18:09 -0500

    ajustes en traduccion
    ajustes en traduccion

--- app/src/main/java/com/example/atanke/traducirpalabras/services/TraducirPalabraService.java
+++ app/src/main/java/com/example/atanke/traducirpalabras/services/TraducirPalabraService.java
@@ -1,10 +1,8 @@
 package com.example.atanke.traducirpalabras.services;
 
-import com.example.atanke.traducirpalabras.models.TraducirPalabraRequest;
 import com.example.atanke.traducirpalabras.models.TraducirPalabraResponse;
 
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
@@ -13,6 +11,6 @@ public interface TraducirPalabraService {
 
     @FormUrlEncoded
     @POST("api/traducir")
-    Call<TraducirPalabraResponse> getTraducir(@Field("data") String data);
+    Call<TraducirPalabraResponse> getTraducir(@Field("data") String data,@Field("fk_idioma") String fk_idioma);
 
 }
